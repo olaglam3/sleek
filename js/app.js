b9635c2548793252d64e158b09a4e22a0d89cec2
olaglam3 <52584780+olaglam3@users.noreply.github.com>
2019-07-11 02:31:07 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -89,7 +89,7 @@ $(document).ready(function() {
   search_field.ghostHunter({
     results: search_results,
     onKeyUp: true,
-    rss: base_url + '/feed.xml',
+    rss: {{ site.baseurl }} + '/search.json',
     result_template : search_result_template,
     zeroResultsInfo : false,
     displaySearchInfo: false,
